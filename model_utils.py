21c57262c0fb50f7cf90993e300fee96a4490b2f
Sudarshan <su0@ornl.gov>
2019-10-21 11:04:03 -0400

    Updated run iters

--- model_utils.py
+++ model_utils.py
@@ -13,7 +13,7 @@ import numpy as np
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-from tqdm import tqdm
+from tqdm import trange
 
 from .data_utils import set_group_splits
 from .tokenizer import tokenize
@@ -25,35 +25,31 @@ def get_best_params(clf, x_train, y_train, param_space, **kwargs):
   
   return param_search.best_params_, pd.DataFrame(param_search.cv_results_)
 
-def run_iters(data_df, clf_model, params, vectorizer, threshold, workdir, prefix, split_pct=0.15, n_iters=100, start_seed=127):
-  targs, preds, probs = [], [], []
-  seeds = list(range(start_seed, start_seed + n_iters))
-  for seed in tqdm(seeds, desc='Run #'):
-    df = set_group_splits(data_df.copy(), group_col='encounter_id', seed=seed, pct=split_pct)
-    train_df = df.loc[df['split'] == 'train', ['note', 'imminent_adm_label']]
-    test_df = df.loc[df['split'] == 'test', ['note', 'imminent_adm_label']]
-    
+def run_iters(data_df, clf_model, clf_params, vectorizer, threshold, start_seed, split_pct=0.15, n_iters=100):
+  clfs, targs, preds, probs = [], [], [], []
+  t = trange(start_seed, start_seed + n_iters, desc='Run #', leave=True)
+  # seeds = list(range(start_seed, start_seed + n_iters))
+
+  for seed in t:
+    t.set_description(f"Run # (seed {seed})")
+    df = set_group_splits(data_df.copy(), group_col='hadm_id', seed=seed, pct=split_pct)
+    train_df = df.loc[df['split'] == 'train', ['note', 'imi_adm_label']]
+    test_df = df.loc[df['split'] == 'test', ['note', 'imi_adm_label']]
+
     x_train = vectorizer.fit_transform(train_df['note'])
     x_test = vectorizer.transform(test_df['note'])    
-    y_train = train_df['imminent_adm_label'].to_numpy()
-    y_test = test_df['imminent_adm_label'].to_numpy()
+    y_train = train_df['imi_adm_label'].to_numpy()
+    y_test = test_df['imi_adm_label'].to_numpy()
     targs.append(y_test)
-    
-    if params:      
-      clf = clf_model(**params)
-    else:
-      clf = pickle.load(clf_model.open('rb'))
 
+    clf = clf_model(**clf_params)
     clf.fit(x_train, y_train)
-    pickle.dump(clf, open((workdir/f'models/{prefix}_seed_{seed}.pkl'), 'wb'))
-    
+    clfs.append(clf)
+
     prob = clf.predict_proba(x_test)
     probs.append(prob)
-    
+
     y_pred = (prob[:, 1] > threshold).astype(np.int64)
     preds.append(y_pred)
-    
-  with open(workdir/f'{prefix}_preds.pkl', 'wb') as f:
-    pickle.dump(targs, f)
-    pickle.dump(probs, f)
-    pickle.dump(preds, f)
+  
+  return clfs, targs, probs, preds
